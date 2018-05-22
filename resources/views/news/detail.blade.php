0aa087763f51b148fc807887b13adb14cfab0d2f
Gio <liu.gio@lexisnexis.com>
2018-05-22 09:27:56 +0800

    News: detail

--- resources/views/news/detail.blade.php
+++ resources/views/news/detail.blade.php
@@ -17,28 +17,27 @@
     </section>
     <!--banner End-->
     <section class="detail_content">
-  <div class="infoShow">
-      <span class="info">宜昌校区</span><span class="date">2011年8月21日</span>
-  </div>
-    <div class="newsCenter">
-        <ul class="newsUl">
-            <li class="item">
-                <h3 class="title">空间发色个个</h3>
-                <div class="news_detail">
-                    <p>齐天大圣跆拳道教育集团成立于2011年，是经过湖北省体育局体教部门审批的正规培训场所。规模庞大，师资力量雄厚，机构教练均由本科院校跆拳道专业毕业，专业技能过硬，是湖北省内具有晋级资历的专业跆拳道馆，是全国跆拳道行业中崛起的标杆。</p>
-                    <img src="/imgs/news/detail.jpg" alt="detail"/>
-                    <p>齐天大圣跆拳道教育集团成立于2011年，是经过湖北省体育局体教部门审批的正规培训场所。规模庞大，师资力量雄厚，机构教练均由本科院校跆拳道专业毕业，专业技能过硬，是湖北省内具有晋级资历的专业跆拳道馆，是全国跆拳道行业中崛起的标杆。</p>
-                </div>
-            </li>
-        </ul>
-        <button type="button" class="blackBtn">返回</button>
-        <div class="pagination">
-            <ul class="pagUl">
-                <li class="item">上一篇：<a href="javascript:;">每次可偶尔间</a></li>
-                <li class="item">下一篇：<a href="javascript:;">你的飞碟</a></li>
-            </ul>
+        <div class="infoShow">
+          <span class="info">宜昌校区</span>
+          <span class="date">{{ date('Y年n月j日', strtotime($detail['created_at'])) }}</span>
         </div>
-    </div>
+        <div class="newsCenter">
+            <ul class="newsUl">
+                <li class="item">
+                    <h3 class="title">{{ $detail['title'] }}</h3>
+                    <div class="news_detail">
+                        <p>{{ htmlspecialchars($detail['content'], ENT_HTML401) }}</p>
+                    </div>
+                </li>
+            </ul>
+            <button type="button" class="blackBtn">返回</button>
+            <div class="pagination">
+                <ul class="pagUl">
+                    <li class="item">上一篇：<a href="javascript:;">每次可偶尔间</a></li>
+                    <li class="item">下一篇：<a href="javascript:;">你的飞碟</a></li>
+                </ul>
+            </div>
+        </div>
     </section>
 </article>
 @endsection
