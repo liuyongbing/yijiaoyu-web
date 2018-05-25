d21ac7ef962c2108a8fcbfef0cbe503836f866db
Gio <liu.gio@lexisnexis.com>
2018-05-25 13:34:28 +0800

    Banner

--- resources/views/home/list.blade.php
+++ resources/views/home/list.blade.php
@@ -8,14 +8,9 @@
         <ul class="bannerUl swiper-wrapper">
             <li class="item swiper-slide">
                 <a href="{{ route('about.index') }}">
-                    <img src="imgs/banners/indexBanner03.jpg{{ $STATIC_VERSION }}" alt="03">
+                    <img src="imgs/banners/indexBanner04.jpg{{ $STATIC_VERSION }}" alt="03">
                 </a>
             </li>
-            <!--li class="item swiper-slide">
-                <a href="javascript:;" target="_blank">
-                    <img src="imgs/banners/indexBanner02.jpg{{ $STATIC_VERSION }}" alt="02">
-                </a>
-            </li-->
             <li class="item swiper-slide">
                 <a href="http://music.163.com/#/song?id=556062363" target="_blank">
                     <img src="imgs/banners/indexBanner01.jpg{{ $STATIC_VERSION }}" alt="01">
