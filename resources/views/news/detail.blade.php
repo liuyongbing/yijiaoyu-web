df75ad8fd77e6e2b3579c79f0f44e8e732b33931
Gio <liu.gio@lexisnexis.com>
2018-06-03 11:51:17 +0800

    News

--- resources/views/news/detail.blade.php
+++ resources/views/news/detail.blade.php
@@ -13,19 +13,19 @@
     
     <section class="detail_content">
         <div class="infoShow">
-          <span class="info">宜昌校区</span>
+          <span class="info">{{ $detail['category_name'] }}</span>
           <span class="date">{{ date('Y年n月j日', strtotime($detail['created_at'])) }}</span>
         </div>
         <div class="newsCenter">
             <ul class="newsUl">
                 <li class="item">
-                    <h3 class="title">{{ $detail['title'] }}</h3>
+                    <h1 class="title">{{ $detail['title'] }}</h1>
                     <div class="news_detail">
                         <p>{!! $detail['content']!!}</p>
                     </div>
                 </li>
             </ul>
-            <button type="button" class="blackBtn">返回</button>
+            <button type="button" class="blackBtn"><a href="{{ route('news.index') }}">返回</a></button>
             <div class="pagination">
                 <ul class="pagUl">
                 @if (!empty($previous))
